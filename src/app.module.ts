de562f5cf9ab19aee075d9198dcc40c7928662d3
sefabaser <sefabaser84@hotmail.com>
2020-09-24 13:07:34 +0200

    refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from 'src/app.controller';
 import { AuthModule } from 'src/auth/auth.module';
 import { BookModule } from 'src/book/book.module';
 import { BooksModule } from 'src/books/books.module';
@@ -7,7 +6,7 @@ import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [AuthModule, UsersModule, BookModule, BooksModule],
-  controllers: [AppController],
+  controllers: [],
   providers: []
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,14 @@
-import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
+import { Controller, UseGuards, Request, Post } from '@nestjs/common';
 import { LocalAuthGuard } from 'src/auth/guards/local-auth.guard';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { AuthService } from 'src/auth/services/auth.service';
 
-@Controller()
-export class AppController {
+@Controller('auth')
+export class AuthController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get()
-  ping(): string {
-    return 'pong';
   }
 }
