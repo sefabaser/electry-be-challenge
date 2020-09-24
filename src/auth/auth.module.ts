d025374c2419f057487d52f9b028db31c6b9ec63
sefabaser <sefabaser84@hotmail.com>
2020-09-24 13:02:57 +0200

    refactoring

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,18 +2,18 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
-import { JwtConstants } from 'src/auth/constants';
 import { LocalStrategy } from 'src/auth/guards/local.strategy';
 import { JwtStrategy } from 'src/auth/guards/jwt.strategy';
 import { AuthService } from 'src/auth/services/auth.service';
+import { Config } from 'src/config';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: JwtConstants.secret,
-      signOptions: { expiresIn: '600s' }
+      secret: Config.secret,
+      signOptions: { expiresIn: Config.loginDuration }
     })
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const Config = {
+  secret: 'secretKey', // TODO: move the secret to the vault
+  loginDuration: '6000s'
+};
