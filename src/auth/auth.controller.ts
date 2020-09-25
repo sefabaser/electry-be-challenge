abed5ee222cff5199e7b2350c9869c96760ca725
sefabaser <sefabaser84@hotmail.com>
2020-09-25 14:01:58 +0200

    implementation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,14 @@
-import { Controller, UseGuards, Req, Post } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Post, Query } from '@nestjs/common';
 
-import { LocalAuthGuard } from 'src/auth/guards/local-auth.guard';
 import { AuthService } from 'src/auth/services/auth.service';
-import { LoginResponse } from 'src/auth/_interfaces';
-import { User } from 'src/users/_interfaces';
+import { LoginRequest, LoginResponse } from 'src/auth/_interfaces';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Req() req: Request): Promise<LoginResponse> {
-    let user = <User>req.user;
-    return this.authService.login(user);
+  async login(@Query() loginRequest: LoginRequest): Promise<LoginResponse> {
+    return this.authService.login(loginRequest);
   }
 }
