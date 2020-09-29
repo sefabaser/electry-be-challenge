dcc45a07a2ce8d71c1125bbee7c58ca86577c0cc
sefabaser <sefabaser84@hotmail.com>
2020-09-29 14:43:03 +0200

    minor update

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -3,14 +3,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 
 import { AppModule } from 'src/app.module';
-import { UnitTestHelper } from 'src/utilities/unit-test.helper';
+import { MockUser, UnitTestHelper } from 'src/utilities/unit-test.helper';
 import { UsersService } from 'src/users/services/users.service';
-
-const SampleUser = {
-  userId: 1,
-  username: 'Lohgarra',
-  password: 'wookieSidearm'
-};
 
 describe('Book Controller', () => {
   let app: INestApplication;
@@ -24,7 +18,7 @@ describe('Book Controller', () => {
 
     // TODO: update to "fake db connection" after having the db
     let usersService = moduleFixture.get(UsersService);
-    (<any>usersService).users = [SampleUser];
+    (<any>usersService).users = [MockUser];
 
     app = await UnitTestHelper.initTestApp(moduleFixture);
   });
@@ -32,14 +26,14 @@ describe('Book Controller', () => {
   it('POST: /auth/login', () => {
     return request(app.getHttpServer())
       .post('/auth/login')
-      .query({ username: SampleUser.username, password: SampleUser.password })
+      .query({ username: MockUser.username, password: MockUser.password })
       .expect(201);
   });
 
   it('POST: /auth/login, not valid password', () => {
     return request(app.getHttpServer())
       .post('/auth/login')
-      .query({ username: SampleUser.username, password: 'wrongPass' })
+      .query({ username: MockUser.username, password: 'wrongPass' })
       .expect(401);
   });
 });
